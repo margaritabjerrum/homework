0e6b42d43c9a711eb5db830b1b3c65248ff61b79
margaritabjerrum <margarita.juskyte@gmail.com>
2023-06-09 14:56:09 +0300

    docs: readme updated

--- src/pages/timeline-display/data-grid-table.js
+++ src/pages/timeline-display/data-grid-table.js
@@ -1,20 +1,20 @@
 import { DataGrid } from '@mui/x-data-grid';
 import React from 'react';
 
-const DataGridTable = ({rows, columns, processRowUpdate}) => {
+const DataGridTable = ({ rows, columns, processRowUpdate }) => {
 
   return (
     <DataGrid
-    rows={rows}
-    columns={columns}
-    processRowUpdate={processRowUpdate}
-    pageSizeOptions={[7, 14, 21, 28]}
-    initialState={{
-      pagination: {
-        paginationModel: { pageSize: 7, page: 0 },
-      },
-    }}
-  />
+      rows={rows}
+      columns={columns}
+      processRowUpdate={processRowUpdate}
+      pageSizeOptions={[7, 14, 21, 28]}
+      initialState={{
+        pagination: {
+          paginationModel: { pageSize: 7, page: 0 },
+        },
+      }}
+    />
   )
 }
 
